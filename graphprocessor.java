9d74c8fc24508e93848e9502976e97fe9701da23
xjg <2644817432@qq.com>
2024-06-02 23:03:28 +0800

    Changes in B1 branch

--- graphprocessor.java
+++ graphprocessor.java
@@ -21,10 +21,7 @@ public class graphprocessor {
             processor.generateGraphFromFile(filePath);
             System.out.println("有向图已经生成成功！");
             visualizeGraph(processor.generateGraphText());
-          
-
-
-            
+        
             while (true) {
                 System.out.println("\n请选择功能：");
                 System.out.println("1. 展示有向图");
